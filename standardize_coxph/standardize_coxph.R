b9d5e265b8991928b954d994dbde73c946c89001
Timbul Widodo, S.TP <160803827+timbulwidodostp@users.noreply.github.com>
2025-07-18 08:12:22 +0700

    standardize_coxph (stdReg2) With (In) R Software

--- standardize_coxph/standardize_coxph.R
+++ standardize_coxph/standardize_coxph.R
@@ -5,7 +5,7 @@
 install.packages("stdReg2")
 library("stdReg2")
 require(survival)
-standardize_coxph = read.csv("https://raw.githubusercontent.com/timbulwidodostp/standardize_coxph/main/spml/spml_one.csv",sep = ";")
+standardize_coxph = read.csv("https://raw.githubusercontent.com/timbulwidodostp/standardize_coxph/main/standardize_coxph/standardize_coxph.csv",sep = ";")
 # Estimation Regression standardization in Cox proportional hazards models Use standardize_coxph (stdReg2) With (In) R Software
 standardize_coxph <- standardize_coxph(formula = Surv(U, D) ~ X + Z + X * Z, data = standardize_coxph, values = list(X = seq(-1, 1, 0.5)), times = 1:5)
 standardize_coxph
